a2e49fabdd8b83710e4c6e39d98710450fbdf034
Jimena Hernández <her21199@uvg.edu.gt>
2023-09-08 09:39:43 -0600

    final

--- src/pages/PrincipalPage.jsx
+++ src/pages/PrincipalPage.jsx
@@ -12,14 +12,14 @@ const PrincipalPage = () => {
                 <div className={styles.containerBodyAudi}>
                     <img src="/images/LOGO-AUDI.svg" alt="logo" />
                     <div className={styles.containerButton}>
-                        <button className={styles.button} style={{ color: "#9e9e9e" }}>Ingresa</button>
+                        <button className={styles.button} style={{ color: "#9e9e9e" }} onClick={() => { window.location.href = "https://audiguatemala.com/" }}>Ingresa</button>
                         <img src="/images/arrowgray.svg" alt="logo" />
                     </div>
                 </div>
                 <div className={styles.containerBodyDucati}>
                     <img src="/images/DUCATI-LOGO.svg" alt="logo" />
                     <div className={styles.containerButton}>
-                        <button className={styles.button} style={{ color: "#bd0000" }}>Ingresa</button>
+                        <button className={styles.button} style={{ color: "#bd0000" }} onClick={() => { window.location.href = "https://ducatiguatemala.com/" }}>Ingresa</button>
                         <img src="/images/arrowred.svg" alt="logo" />
                     </div>
                 </div>
